d66e56e27d6784de70a9cf54e3138fdf0140286b
Russel <170238499+Wrul7@users.noreply.github.com>
2025-01-05 13:16:04 +0800

    Style boxes and main interface via CSS

--- javascript.js
+++ javascript.js
@@ -1,5 +1,4 @@
 let square_per_side = 16;
-// let grid_num = square_num_per_side ** 2;
 
 const container = document.querySelector(".box_container");
 
@@ -15,11 +14,3 @@ for (let i = 1; i <= square_per_side; i++) {
 
     container.appendChild(create_row);
 }
-
-
-// const square = document.querySelectorAll(".square");
-// const row = document.querySelectorAll(".row");
-
-// container.setAttribute("style", "display: flex; flex-direction: column;");
-// square.setAttribute("style", "border-color: black;");
-// row.setAttribute("style", "display: flex; width: 50%; padding-bottom: 50%");
